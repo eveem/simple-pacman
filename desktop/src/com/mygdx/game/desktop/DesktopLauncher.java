4354ebed9452db32a03d501ff5301adc5e914731
eveem <monpriya.tammavong@gmail.com>
2016-09-23 16:04:16 +0700

    Test git

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.PacmanGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
+		config.width = 200;
 		config.height = 800;
 		new LwjglApplication(new PacmanGame(), config);
 	}
