87dc22d676899e5c55eb52a507426f43857df62c
eveem <monpriya.tammavong@gmail.com>
2016-09-23 16:02:02 +0700

    Config width

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,7 +7,7 @@ import com.mygdx.game.PacmanGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 400;
+		config.width = 800;
 		config.height = 800;
 		new LwjglApplication(new PacmanGame(), config);
 	}
