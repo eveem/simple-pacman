31c4595be7ca66db57918cb684eedacb1279842d
eveem <monpriya.tammavong@gmail.com>
2016-10-06 23:27:18 +0700

    Assign SPEED and check dir

--- core/src/com/mygdx/game/Pacman.java
+++ core/src/com/mygdx/game/Pacman.java
@@ -10,6 +10,7 @@ public class Pacman {
     public static final int DIRECTION_UP = 3;
     public static final int DIRECTION_LEFT = 4;
     public static final int DIRECTION_STILL = 0;
+    public static final int SPEED = 10;
 	 
     private static final int [][] DIR_OFFSETS = new int [][] {
         {0,0},
@@ -28,8 +29,9 @@ public class Pacman {
     }
 	
     public void move(int dir) { 
-
-        position.x += 10 * DIR_OFFSETS[dir][0];
-        position.y += 10 * DIR_OFFSETS[dir][1];
+    	if (dir >= 0 && dir <= 4) {
+    		position.x += SPEED * DIR_OFFSETS[dir][0];
+    		position.y += SPEED * DIR_OFFSETS[dir][1];
+    	}
     }
 }
