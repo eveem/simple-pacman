9e7e7a30b25a7ddd20f3bcb83cffbdcf8a8b1cff
eveem <monpriya.tammavong@gmail.com>
2016-10-06 23:57:39 +0700

    Finish class World and WorldRenderer

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,52 +1,27 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 
 public class GameScreen extends ScreenAdapter {
 	
-	private PacmanGame pacmanGame;
-	private Texture pacmanImg;
-	private Pacman pacman;
+	private WorldRenderer worldRenderer;
+	private World world;
 	
 	public GameScreen(PacmanGame pacmanGame) {
-        this.pacmanGame = pacmanGame;
-        pacmanImg = new Texture("pacman.png");
-        pacman = new Pacman(100,100);
-    }
-	
-	private void update(float delta) {
-		if(Gdx.input.isKeyPressed(Keys.UP)) {
-            pacman.move(Pacman.DIRECTION_UP);
-        }
-        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-        	pacman.move(Pacman.DIRECTION_RIGHT);
-        }
-        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-        	pacman.move(Pacman.DIRECTION_DOWN);
-        }
-        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-        	pacman.move(Pacman.DIRECTION_LEFT);
-        }  
-    }
+        world = new World(pacmanGame);
+        worldRenderer = new WorldRenderer(pacmanGame, world);
+	}
 	
 	@Override
     public void render(float delta) {
-		update(delta);
+		world.update(delta);
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
-        SpriteBatch batch = pacmanGame.batch;
-        batch.begin();
-        Vector2 pos = pacman.getPosition();
-        batch.draw(pacmanImg, pos.x, pos.y);
-        batch.end();
+        worldRenderer.render(delta);
     }
 
 	
--- core/src/com/mygdx/game/World.java
+++ core/src/com/mygdx/game/World.java
@@ -1,5 +1,33 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+
 public class World {
-	
+	private Pacman pacman;
+    private PacmanGame pacmanGame;
+ 
+    World(PacmanGame pacmanGame) {
+        this.pacmanGame = pacmanGame;
+        pacman = new Pacman(100,100);
+    }
+ 
+    Pacman getPacman() {
+        return pacman;
+    }
+    
+    public void update(float delta) {
+		if(Gdx.input.isKeyPressed(Keys.UP)) {
+            pacman.move(Pacman.DIRECTION_UP);
+        }
+        if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+        	pacman.move(Pacman.DIRECTION_RIGHT);
+        }
+        if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+        	pacman.move(Pacman.DIRECTION_DOWN);
+        }
+        if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+        	pacman.move(Pacman.DIRECTION_LEFT);
+        }  
+    }
 }
