5fdd26d040a6a6474727fb860c76989f6a4d7222
eveem <monpriya.tammavong@gmail.com>
2016-10-07 13:32:55 +0700

    Can show wall and dot

--- core/src/com/mygdx/game/MazeRenderer.java
+++ core/src/com/mygdx/game/MazeRenderer.java
@@ -5,18 +5,32 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MazeRenderer {
 
-	private Texture wallImg, dotImg;
+	private Texture wallImage, dotImage;
 	private Maze maze;
     private SpriteBatch batch;
  
     public MazeRenderer(SpriteBatch batch, Maze maze) {
         this.maze = maze;
         this.batch = batch;
-        wallImg = new Texture("wall.png");
-        dotImg = new Texture("dot.png");
+        wallImage = new Texture("wall.png");
+        dotImage = new Texture("dot.png");
     }
  
     public void render() {
+    	batch.begin();
+    	for(int r = 0; r < maze.getHeight(); r++) {
+            for(int c = 0; c < maze.getWidth(); c++) {
+                int x = c * 40;
+                int y = PacmanGame.HEIGHT - (r * 40) - 40;
+ 
+                if(maze.hasWallAt(r, c)) {
+                    batch.draw(wallImage, x, y);
+                } else if(maze.hasDotAt(r, c)) {
+                    batch.draw(dotImage, x, y);
+                }
+            }
+        }
+        batch.end();
     }
 	
 }
